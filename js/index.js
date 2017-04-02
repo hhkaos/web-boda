bcae6f604436b5db0e2b06f83a6581d39e9d2799
RaulJimenezOrtega <hhkaos@gmail.com>
2017-04-02 18:06:29 +0200

    Adding google

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-var map = L.map("mapDiv").setView([37.247846, -1.869034], 16);
+var map = L.map("mapDiv").setView([37.247846, -1.869034], 17);
 
-L.esri.basemapLayer("Imagery").addTo(map);
+//L.esri.basemapLayer("Imagery").addTo(map);
 
 var markers = L.esri.featureLayer({
   url: 'http://services.arcgis.com/Q6ZFRRvMTlsTTFuP/arcgis/rest/services/Boda_points/FeatureServer/0'
 }).addTo(map);
 var MyBingMapsKey = 'Ap2XquXVzw7hLotGj1q_Fz8cKui5p3ECQ9zyjp9sRK4ckrZYoPc-9w13KBXex0dP';
-L.tileLayer.bing(MyBingMapsKey).addTo(map)
+//L.tileLayer.bing(MyBingMapsKey).addTo(map)
+var roads = L.gridLayer.googleMutant({
+    type: 'satellite' // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+}).addTo(map);
 
 markers.bindPopup(function (layer) {
     return L.Util.template('<p>Test</p>', layer.feature.properties);
